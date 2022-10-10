b96fc498277a153ff6ebc91bed149a791cf12771
glenda0404 <113426635+glenda0404@users.noreply.github.com>
2022-10-10 23:35:48 +0800

    Update lab05.py

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -7,19 +7,23 @@ dict0={'index':list1,
        'StuB':list3,
        'StuC':list4}
 print (dict0)
-A = (int(list2[0])+int(list2[1])+int(list2[2])+int(list2[3])+int(list2[4]))/5 #A's average
-B = (int(list3[0])+int(list3[1])+int(list3[2])+int(list3[3])+int(list3[4]))/5 #B's average
-C = (int(list4[0])+int(list4[1])+int(list4[2])+int(list4[3])+int(list4[4]))/5 #C's average
-print("A學生平均成績："+str(A))#print A's average
-print("B學生平均成績："+str(B))#print B's average
-print("C學生平均成績："+str(C))#print C's average
-chi = (int(list2[0])+int(list3[0])+int(list4[0]))/3#chinese's average
-eng = (int(list2[1])+int(list3[1])+int(list4[1]))/3#english's average
-math = (int(list2[2])+int(list3[2])+int(list4[2]))/3#math's average
-sci = (int(list2[3])+int(list3[3])+int(list4[3]))/3#science's average
-soc = (int(list2[4])+int(list3[4])+int(list4[4]))/3#social's average
-print("國文平均成績："+str(chi))#print chinese's average
-print("英文平均成績："+str(eng))#print english's average
-print("數學平均成績："+str(math))#print math's average
-print("自然平均成績："+str(sci))#print science's average
-print("社會平均成績："+str(soc))#print social's average
+#各學生平均
+A = (int(list2[0])+int(list2[1])+int(list2[2])+int(list2[3])+int(list2[4]))/5 
+B = (int(list3[0])+int(list3[1])+int(list3[2])+int(list3[3])+int(list3[4]))/5
+C = (int(list4[0])+int(list4[1])+int(list4[2])+int(list4[3])+int(list4[4]))/5
+#印出平均
+print("A學生平均成績："+str(A))
+print("B學生平均成績："+str(B))
+print("C學生平均成績："+str(C))
+#計算各科平均
+chi = (int(list2[0])+int(list3[0])+int(list4[0]))/3
+eng = (int(list2[1])+int(list3[1])+int(list4[1]))/3
+math = (int(list2[2])+int(list3[2])+int(list4[2]))/3
+sci = (int(list2[3])+int(list3[3])+int(list4[3]))/3
+soc = (int(list2[4])+int(list3[4])+int(list4[4]))/3
+#印出各科平均
+print("國文平均成績："+str(chi))
+print("英文平均成績："+str(eng))
+print("數學平均成績："+str(math))
+print("自然平均成績："+str(sci))
+print("社會平均成績："+str(soc))
